48278257b78e46efd84306ef50fe5084d0c23501
Leonardo Lopes <leobo215@hotmail.com>
2025-03-05 03:13:25 -0300

    Lista de Usuários
    Página onde mostra todos os Usuários do banco de dados

--- src/containers/Lista/lista.jsx
+++ src/containers/Lista/lista.jsx
@@ -1,8 +1,39 @@
-function ListarUsuarios(){
+import { useEffect, useState } from "react"
+import api from "../../services/api"
 
-    return(
+
+
+function ListarUsuarios() {
+    const [allUsers, setAllUsers] = useState([])
+
+    useEffect(() => {
+        async function loadUsers() {
+
+            const token = localStorage.getItem('token')
+            const { data: { users } } = await api.get('/listar-usuarios', {
+                headers: { Authorization: `Bearer ${token}` }
+            })
+
+            setAllUsers(users)
+
+        }
+
+        loadUsers()
+    }, [])
+
+
+    return (
         <div>
-            <h2>Listar Usuários</h2>
+            <h2>Lista de Usuários</h2>
+        <ul>
+            {allUsers.map( user =>
+                <li key={user.id}>
+                    <p>{user.id}</p>
+                    <p>{user.name}</p>
+                    <p>{user.email}</p>
+                </li>
+            )}
+        </ul>
         </div>
     )
 }
