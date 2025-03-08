9f6bcef86783a518e287cc7c3748f3a2c661a925
Leonardo Lopes <leobo215@hotmail.com>
2025-03-07 23:36:51 -0300

    Update Dashboard

--- src/containers/dashboard/dashboard.jsx
+++ src/containers/dashboard/dashboard.jsx
@@ -4,10 +4,21 @@ import './dashboard.css'
 
 function Dashboard() {
     return (
-      <div>
-        <h1>Bem-vindo ao meu aplicativo</h1>
-      </div>
+        <main className='container'>
+            <div className='card'>
+                <h3>RH</h3>
+            </div>
+            <div className='card'>
+                <h3>ALMOXARIFADO</h3>
+            </div>
+            <div className='card'>
+                <h3>COMPRAS</h3>
+            </div>
+            <div className='card'>
+                <h3>NACIONALIZADO</h3>
+            </div>
+        </main>
     );
-  }
+}
 
 export default Dashboard
