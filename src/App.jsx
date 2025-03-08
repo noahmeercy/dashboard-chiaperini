0e78dbbd0f418169e0185468e09d66755deb0df0
Leonardo Lopes <leobo215@hotmail.com>
2025-03-07 22:46:55 -0300

    Dashboard
    Iniciando o desenvolvimento da DASHBOARD

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import Login from './containers/Login/login'
 import Cadastro from './containers/Cadastro/cadastro'
 import ListarUsuarios from "./containers/Lista/lista"
+import Dashboard from "./containers/dashboard/dashboard"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/" element={<Login/>} />
                 <Route path="/Cadastro" element={<Cadastro/>} />
                 <Route path="/listar-usuarios" element={<ListarUsuarios/>} />
+                <Route path="/dashboard" element={<Dashboard/>} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/containers/dashboard/dashboard.jsx
@@ -0,0 +1,13 @@
+import './dashboard.css'
+
+
+
+function Dashboard() {
+    return (
+      <div>
+        <h1>Bem-vindo ao meu aplicativo</h1>
+      </div>
+    );
+  }
+
+export default Dashboard
