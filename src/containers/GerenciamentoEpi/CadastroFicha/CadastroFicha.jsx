7d944554e46c468d1fe4ebfea07f93c8f404e059
Leonardo Lopes <leobo215@hotmail.com>
2025-03-11 02:21:27 -0300

    Update

--- src/containers/GerenciamentoEpi/CadastroFicha/CadastroFicha.jsx
+++ src/containers/GerenciamentoEpi/CadastroFicha/CadastroFicha.jsx
@@ -1,13 +1,38 @@
 import "./CadastroFicha.css"
 
 
-function CadastroFicha () {
+function CadastroFicha() {
     return (
-        
-            <div className="TESTE">
-                <p>TESTE</p>
-            </div>
-        
+
+        <div className="container-ficha">
+            <form>
+                <div className='input-box'>
+                    <input placeholder='Nome do Funcionário' type='text' />
+                </div>
+                <div className="input-box">
+                    <input placeholder='Número do Registro' type="number" />
+                </div>
+                <div className="input-box">
+                    <input placeholder='Data de admissão' type="date" />
+                </div>
+                <div className="input-box">
+                    <input placeholder='Função' type="text" />
+                </div>
+                <div className="input-box">
+                    <input placeholder='Setor' type="text" />
+                </div>
+                <div className="input-box">
+                    <input placeholder='Tamanho da Camisa' type="text" />
+                </div>
+                <div className="input-box">
+                    <input placeholder='Tamanho da Calça' type="text" />
+                </div>
+                <div className="input-box">
+                    <input placeholder='Tamanho do Calçado' type="number" />
+                </div>
+            </form>
+        </div>
+
     )
 }
 
