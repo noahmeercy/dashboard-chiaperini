b7fecbdbec83164e5ad111fb002e7d09b28b1df7
Leonardo Lopes <leobo215@hotmail.com>
2025-03-10 23:30:43 -0300

    Update GCE

--- src/containers/GerenciamentoEpi/gerenciamentoepi.jsx
+++ src/containers/GerenciamentoEpi/gerenciamentoepi.jsx
@@ -1,12 +1,15 @@
 import "./gerenciamentoepi.css"
+import { Link } from "react-router-dom"
 
 
 
 
 function GerenciamentoEpi () {
     return (
-        <main className="teste0101">
-            <h1>TESTE</h1>
+        <main className="container-epi">
+            <div className="button-epi">
+                <p>CADASTRAR FICHA</p>
+            </div>
         </main>
     )
 }
