f91e0b5bd9c2868e770d31f0941dba7f69a7d4c8
Leonardo Lopes <leobo215@hotmail.com>
2025-03-12 23:03:08 -0300

    Update

--- src/containers/GerenciamentoEpi/gerenciamentoepi.jsx
+++ src/containers/GerenciamentoEpi/gerenciamentoepi.jsx
@@ -7,9 +7,11 @@ import { Link } from "react-router-dom"
 function GerenciamentoEpi () {
     return (
         <main className="container-epi">
+            <Link to="/cadastro-ficha">
             <div className="button-epi">
                 <p>CADASTRAR FICHA</p>
             </div>
+            </Link>
         </main>
     )
 }
