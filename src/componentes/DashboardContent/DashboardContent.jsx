679d561093c1645b7b26b30b55a2b25a2d6ee007
Leonardo Lopes <leobo215@hotmail.com>
2025-05-08 21:05:24 -0300

    Create Dashboard
    Criando componente para o mostrar o conteúdo do dashboard. (ATALHOS NA TELA)

---
+++ src/componentes/DashboardContent/DashboardContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function DashboardContent() {
+    return (
+        <div>
+            <h1 className='tw-text-2xl tw-font-bold tw-mb-4'>Dashboard</h1>
+            <p></p>
+        </div>
+    )
+}
+
+
+export default DashboardContent;  
