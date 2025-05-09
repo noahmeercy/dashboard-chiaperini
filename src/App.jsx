5dfba2a0b42c1da3fb7f479f3cfead3ce7ab38e7
Leonardo Lopes <leobo215@hotmail.com>
2025-05-08 21:04:29 -0300

    Update App.jsx
    Organizando as rotas para aplicar a nova estrutura.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import Login from './containers/Login/login'
 import Cadastro from './containers/Cadastro/cadastro'
 import ListarUsuarios from "./containers/Lista/lista"
-import DashboardHome from "./containers/DashboardHome/dashboardHome"
+// import DashboardHome from "./containers/DashboardHome/dashboardHome"
+import DashboardContent from './componentes/DashboardContent/DashboardContent';
 import AlmoxarifadoHome from "./containers/AlmoxarifadoHome/almoxarifadohome"
 import GerenciamentoEpi from "./containers/GerenciamentoEpi/gerenciamentoepi"
 import CadastroFicha from "./containers/GerenciamentoEpi/CadastroFicha/CadastroFicha"
@@ -10,23 +11,25 @@ import CadastroEpi from "./containers/GerenciamentoEpi/CadastroEpi/CadastroEpi"
 import TrocarEpi from "./containers/GerenciamentoEpi/TrocarEpi/trocarepi"
 import TabelaTroca from "./componentes/TabelaTroca"
 
-
+import Layout from "./componentes/Layout/Layout";
 
 function App() {
 
     return (
         <BrowserRouter>
-            <Routes>
+            <Routes>  
                 <Route path="/" element={<Login/>} />
                 <Route path="/Cadastro" element={<Cadastro/>} />
-                <Route path="/listar-usuarios" element={<ListarUsuarios/>} />
-                <Route path="/dashboard-home" element={<DashboardHome/>} />
-                <Route path="/almoxarifado-home" element={<AlmoxarifadoHome/>} />
-                <Route path="/gerenciamento-epi" element={<GerenciamentoEpi/>} />
-                <Route path="/cadastro-ficha" element={<CadastroFicha/>} />
-                <Route path="/cadastro-epi" element={<CadastroEpi/>} />
-                <Route path="/trocar-epi" element={<TrocarEpi/>} />
-                <Route path="/tabela-troca" element={<TabelaTroca exibirFiltros={true}/>} />
+
+
+                <Route path="/listar-usuarios" element={<Layout><ListarUsuarios/></Layout>} />
+                <Route path="/dashboard-home" element={<Layout><DashboardContent/></Layout>} />
+                <Route path="/almoxarifado-home" element={<Layout><AlmoxarifadoHome/></Layout>} />
+                <Route path="/gerenciamento-epi" element={<Layout><GerenciamentoEpi/></Layout>} />
+                <Route path="/cadastro-ficha" element={<Layout><CadastroFicha/></Layout>} />
+                <Route path="/cadastro-epi" element={<Layout><CadastroEpi/></Layout>} />
+                <Route path="/trocar-epi" element={<Layout><TrocarEpi/></Layout>} />
+                <Route path="/tabela-troca" element={<Layout><TabelaTroca exibirFiltros={true}/></Layout>} />
             </Routes>
         </BrowserRouter>
     )
